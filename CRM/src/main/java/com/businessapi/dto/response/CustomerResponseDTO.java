07ba669e9aef213f00e50705cd79372090f2b52e
Serkan Guner <serkan.guner@yahoo.com>
2024-09-16 13:57:47 +0300

    auth icin rabbit key degisti security izin artirildi.

--- CRM/src/main/java/com/businessapi/dto/response/CustomerResponseDTO.java
+++ CRM/src/main/java/com/businessapi/dto/response/CustomerResponseDTO.java
@@ -3,5 +3,5 @@ package com.businessapi.dto.response;
 import lombok.Builder;
 
 @Builder
-public record CustomerResponseDTO(String firstName, String lastName, String email, String phone, String address) {
+public record CustomerResponseDTO(Long id,String firstName, String lastName, String email, String phone, String address) {
 }
