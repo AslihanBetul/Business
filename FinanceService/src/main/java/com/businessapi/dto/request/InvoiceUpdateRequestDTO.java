0388735f9a79123898dc58087a0929c49f6dae2c
keremoktener <k.keremoktener@gmail.com>
2024-09-16 11:01:27 +0300

    RabbitMQ and security integration of financeservice

---
+++ FinanceService/src/main/java/com/businessapi/dto/request/InvoiceUpdateRequestDTO.java
@@ -0,0 +1,17 @@
+package com.businessapi.dto.request;
+
+import com.businessapi.entity.enums.EInvoiceStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record InvoiceUpdateRequestDTO(
+        Long id,
+        Long customerIdOrSupplierId,
+        LocalDateTime invoiceDate,
+        BigDecimal totalAmount,
+        BigDecimal paidAmount,
+        EInvoiceStatus invoiceStatus,
+        String description
+) {
+}
--- FinanceService/src/main/java/com/financeservice/repositories/TaxRepository.java
+++
@@ -1,7 +0,0 @@
-package com.financeservice.repositories;
-
-import com.financeservice.entity.Tax;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaxRepository extends JpaRepository<Tax, Long> {
-}
