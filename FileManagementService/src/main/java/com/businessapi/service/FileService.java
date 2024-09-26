3116b1a3da3808be9c47861823091502f09c0b0f
aslihanbetul <aslihan.mert19@gmail.com>
2024-09-27 01:05:06 +0300

    Temel CRUD işlemleri tamamlandı

---
+++ FileManagementService/src/main/java/com/businessapi/service/FileService.java
@@ -0,0 +1,159 @@
+package com.businessapi.service;
+
+
+import com.businessapi.config.ConfigProperties;
+import com.businessapi.dto.request.DeleteFileRequestDTO;
+import com.businessapi.dto.request.SaveFileRequestDTO;
+import com.businessapi.dto.request.UpdateFileRequestDTO;
+import com.businessapi.entity.File;
+import com.businessapi.repository.FileRepository;
+import com.businessapi.utilty.enums.EContentType;
+import com.businessapi.utilty.enums.EStatus;
+import io.minio.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class FileService {
+    private final FileRepository fileRepository;
+    private final MinioClient minioClient;
+    private final ConfigProperties configProperties;
+
+
+
+    public String save(SaveFileRequestDTO dto) {
+        String bucketName = configProperties.getBucket();
+        String uuid = UUID.randomUUID().toString();
+
+        try {
+
+            MultipartFile file = dto.file();
+            EContentType contentType = dto.contentType();
+
+            boolean isExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+
+            if (!isExist) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            }
+
+
+            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
+                    .object(uuid)
+                    .stream(file.getInputStream(), file.getSize(), -1)
+                    .contentType(contentType.getType())
+                    .bucket(bucketName)
+                    .build();
+            minioClient.putObject(putObjectArgs);
+
+
+            File fileEntity = File.builder()
+                    .uuid(uuid)
+                    .authId(1L)
+                    .status(EStatus.ACTIVE)
+                    .contentType(contentType)
+                    .build();
+            fileRepository.save(fileEntity);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return uuid;
+    }
+
+
+
+    public void deleteFile(DeleteFileRequestDTO dto) {
+        try {
+
+            minioClient.removeObject(RemoveObjectArgs.builder()
+                    .bucket(dto.bucketName())
+                    .object(dto.uuid())
+                    .build());
+
+            File file = fileRepository.findByUuid(dto.uuid()).orElseThrow(() -> new RuntimeException("File not found"));
+            file.setStatus(EStatus.DELETED);
+            fileRepository.save(file);
+
+
+        } catch (Exception e) {
+            System.out.println("\n" + "Unexpected error occurred " + e.getMessage());
+        }
+
+    }
+
+    public String updateFile(UpdateFileRequestDTO dto) {
+        try {
+
+            File fileMetadata = getFileMetadata(dto.uuid());
+
+            if (fileMetadata.getStatus() == EStatus.DELETED) {
+                throw new RuntimeException("Silinmiş dosya güncellenemez.");
+            }
+
+
+            minioClient.removeObject(RemoveObjectArgs.builder()
+                    .bucket(configProperties.getBucket())
+                    .object(dto.uuid())
+                    .build());
+
+
+            InputStream fileInputStream = dto.file().getInputStream();
+            minioClient.putObject(PutObjectArgs.builder()
+                    .bucket(configProperties.getBucket())
+                    .object(dto.uuid())
+                    .stream(fileInputStream, fileInputStream.available(), -1)
+                    .contentType(dto.contentType().getType())
+                    .build());
+
+
+            fileMetadata.setUpdateAt(LocalDateTime.now());
+            fileRepository.save(fileMetadata);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Beklenmeyen bir hata: " + e.getMessage());
+        }
+        return dto.uuid();
+    }
+
+
+    public InputStream getFile(String uuid)  {
+
+        File existingFile = fileRepository.findByUuid(uuid)
+                .orElseThrow(() -> new RuntimeException("Dosya bulunamadı"));
+
+        if (existingFile.getStatus() == EStatus.DELETED) {
+            throw new RuntimeException("Bu dosya silinmiştir.");
+        }
+        try {
+            GetObjectArgs getObjectArgs = GetObjectArgs.builder()
+                    .bucket(configProperties.getBucket())
+                    .object(uuid)
+                    .build();
+            return minioClient.getObject(getObjectArgs);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+
+        }
+    }
+    public File getFileMetadata(String uuid) {
+        return fileRepository.findByUuid(uuid)
+                .orElseThrow(() -> new RuntimeException("Dosya bulunamadı, uuid: " + uuid));
+    }
+
+
+
+
+}
+
+
