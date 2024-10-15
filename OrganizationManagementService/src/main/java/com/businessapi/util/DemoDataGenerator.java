588d5ba722ce2b95806de87b6522f21d890f9dce
candenizgumus <denizgumus1996@hotmail.com>
2024-10-15 08:43:17 +0300

    Deleted Manager and related services.

--- OrganizationManagementService/src/main/java/com/businessapi/util/DemoDataGenerator.java
+++ OrganizationManagementService/src/main/java/com/businessapi/util/DemoDataGenerator.java
@@ -3,11 +3,9 @@ package com.businessapi.util;
 
 import com.businessapi.dto.request.DepartmentSaveRequestDto;
 import com.businessapi.dto.request.EmployeeSaveRequestDto;
-import com.businessapi.dto.request.ManagerSaveRequestDto;
 import com.businessapi.entities.Employee;
 import com.businessapi.services.DepartmentService;
 import com.businessapi.services.EmployeeService;
-import com.businessapi.services.ManagerService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,13 +18,11 @@ public class DemoDataGenerator
 {
     private final DepartmentService departmentService;
     private final EmployeeService employeeService;
-    private final ManagerService managerService;
 
     @PostConstruct
     public void generateDemoData()
     {
         departmentDemoData();
-        //managerDemoData();
         employeeDemoData();
 
 
@@ -72,22 +68,6 @@ public class DemoDataGenerator
         employeeService.save(employee4);
     }
 
-    /*private void managerDemoData()
-    {
-
-        managerService.saveForDemoData(new ManagerSaveRequestDto(1L, "12345678910", "05312345678", "John", "Doe", "jdoe@gmail.com"));
-        managerService.saveForDemoData(new ManagerSaveRequestDto(2L, "22345678910", "05322345678", "Jane", "Smith", "jsmith@gmail.com"));
-        managerService.saveForDemoData(new ManagerSaveRequestDto(3L, "32345678910", "05332345678", "Alice", "Johnson", "ajohnson@gmail.com"));
-        managerService.saveForDemoData(new ManagerSaveRequestDto(4L, "42345678910", "05342345678", "Bob", "Williams", "bwilliams@gmail.com"));
-        managerService.saveForDemoData(new ManagerSaveRequestDto(5L, "52345678910", "05352345678", "Charlie", "Brown", "cbrown@gmail.com"));
-        managerService.saveForDemoData(new ManagerSaveRequestDto(6L, "62345678910", "05362345678", "David", "Jones", "djones@gmail.com"));
-        managerService.saveForDemoData(new ManagerSaveRequestDto(7L, "72345678910", "05372345678", "Emily", "Davis", "edavis@gmail.com"));
-        managerService.saveForDemoData(new ManagerSaveRequestDto(8L, "82345678910", "05382345678", "Frank", "Miller", "fmiller@gmail.com"));
-        managerService.saveForDemoData(new ManagerSaveRequestDto(9L, "92345678910", "05392345678", "Grace", "Wilson", "gwilson@gmail.com"));
-        managerService.saveForDemoData(new ManagerSaveRequestDto(10L, "02345678910", "05302345678", "Henry", "Moore", "hmoore@gmail.com"));
-
-
-    }*/
 
 
 }
